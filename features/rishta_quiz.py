2fa4b1d90a2f0781b7d86c24c8b92cf2ef765bc4
Alishba Meraj <164425954+AlishbaMeraj@users.noreply.github.com>
2025-06-01 18:18:15 +0500

    Delete features directory

--- features/rishta_quiz.py
+++
@@ -1,7 +0,0 @@
-# features/rishta_quiz.py
-
-import streamlit as st
-
-def show_rishta_quiz():
-    st.subheader("🎭 Rishta Personality Quiz (Coming Soon)")
-    st.info("This fun quiz will reveal your rishta persona! 🚧 In progress...")
